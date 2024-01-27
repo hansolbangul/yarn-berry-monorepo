6084af10d44b39582c82f8f57018eaddf6a78912
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2024-01-27 12:39:00 +0900

    [FEAT] Yarn Berry Monorepo 환경 구축

---
+++ apps/music-with-memo/src/App.tsx
@@ -0,0 +1,15 @@
+import Input from 'libs/commons/Input';
+import Text from 'libs/commons/Text';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Input />
+      <Text></Text>
+    </>
+  );
+}
+
+export default App;
